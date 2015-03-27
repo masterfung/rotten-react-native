d0d2521f32cb26cf3818e56165ffe9fab7006fad
Tsung Hung <hungmasterj@gmail.com>
2015-03-27 16:53:33 -0700

    using the rotten api to get data

--- RottenTomatoes/index.ios.js
+++ RottenTomatoes/index.ios.js
@@ -8,6 +8,7 @@ var React = require('react-native');
 var {
   AppRegistry,
   Image,
+  ListView,
   StyleSheet,
   Text,
   View,
@@ -18,22 +19,76 @@ var MOCKED_MOVIES_DATA = [
     {thumbnail: "http://vignette4.wikia.nocookie.net/voiceacting/images/a/af/How_to_Train_Your_Dragon_DVD_Cover.jpg/revision/latest?cb=20130512185903"}}
 ];
 
+var API_KEY = 'y2fb7ajxwdkc8h8y22ur7eer';
+var API_URL = 'http://api.rottentomatoes.com/api/public/v1.0/lists/movies/in_theaters.json';
+
+var PAGE_LIMIT = 25;
+var PARAMS = '?apikey=' + API_KEY + '&page_limit=' + PAGE_LIMIT;
+var REQUEST_URL = API_URL + PARAMS;
+
 var RottenTomatoes = React.createClass({
+  getInitialState: function() {
+    return {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      }),
+      loaded: false,
+    };
+  },
+
+  componentDidMount: function() {
+    this.fetchData();
+  },
+
+  fetchData: function() {
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.setState({
+          dataSource: this.state.dataSource.cloneWithRows(responseData.movies),
+          loaded: true,
+        });
+      })
+      .done();
+  },
+
   render: function() {
-    var movie = MOCKED_MOVIES_DATA[0];
+    if (!this.state.loaded) {
+      return this.renderLoadingView();
+    }
+    return (
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={this.renderMovie}
+        style={styles.listView}
+      />
+    );
+  },
+
+  renderLoadingView: function() {
+    return (
+      <View style={styles.container}>
+        <Text>
+          Loading movies...
+        </Text>
+      </View>
+    );
+  },
+
+  renderMovie: function(movie) {
     return (
       <View style={styles.container}>
         <Image
-          source={{uri: movie.posters.thumbnail}}
-          style = {styles.thumbnail}
-          />
+          source={{uri: movie.posters.thumbnail}}.replace('_tmb', '_det')
+          style={styles.thumbnail}
+        />
         <View style={styles.rightContainer}>
           <Text style={styles.title}>{movie.title}</Text>
           <Text style={styles.year}>{movie.year}</Text>
         </View>
       </View>
     );
-  }
+  },
 });
 
 var styles = StyleSheet.create({
@@ -44,17 +99,12 @@ var styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  rightContianer: {
+  rightContainer: {
     flex:1,
   },
   thumbnail: {
-    width: 90,
-    height: 121,
+    width: 53,
+    height: 81,
   },
   title: {
     fontSize: 20,
@@ -63,7 +113,11 @@ var styles = StyleSheet.create({
   },
   year: {
     textAlign: 'center',
-  }
+  },
+  listView: {
+    paddingTop: 20,
+    backgroundColor: '#F5FCFF',
+  },
 });
 
 AppRegistry.registerComponent('RottenTomatoes', () => RottenTomatoes);
