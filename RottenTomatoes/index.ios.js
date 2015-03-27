eb93c7674eef123e280d468f5ffe2d49ef77b875
Tsung Hung <hungmasterj@gmail.com>
2015-03-27 16:17:24 -0700

    added more js to make the data load

--- RottenTomatoes/index.ios.js
+++ RottenTomatoes/index.ios.js
@@ -1,28 +1,36 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
+ * A Very Rotten Tomatoe Test Trial
+
  */
 'use strict';
 
 var React = require('react-native');
 var {
   AppRegistry,
+  Image,
   StyleSheet,
   Text,
   View,
 } = React;
 
+var MOCKED_MOVIES_DATA = [
+  {title: "How to Train Your Dragon", year: "2014", posters:
+    {thumbnail: "http://vignette4.wikia.nocookie.net/voiceacting/images/a/af/How_to_Train_Your_Dragon_DVD_Cover.jpg/revision/latest?cb=20130512185903"}}
+];
+
 var RottenTomatoes = React.createClass({
   render: function() {
+    var movie = MOCKED_MOVIES_DATA[0];
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js{'\n'}
-          Press Cmd+R to reload
-        </Text>
+        <Image
+          source={{uri: movie.posters.thumbnail}}
+          style = {styles.thumbnail}
+          />
+        <View style={styles.rightContainer}>
+          <Text style={styles.title}>{movie.title}</Text>
+          <Text style={styles.year}>{movie.year}</Text>
+        </View>
       </View>
     );
   }
@@ -31,6 +39,7 @@ var RottenTomatoes = React.createClass({
 var styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
@@ -40,10 +49,21 @@ var styles = StyleSheet.create({
     textAlign: 'center',
     margin: 10,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
+  rightContianer: {
+    flex:1,
   },
+  thumbnail: {
+    width: 90,
+    height: 121,
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 8,
+    textAlign: 'center',
+  },
+  year: {
+    textAlign: 'center',
+  }
 });
 
 AppRegistry.registerComponent('RottenTomatoes', () => RottenTomatoes);
